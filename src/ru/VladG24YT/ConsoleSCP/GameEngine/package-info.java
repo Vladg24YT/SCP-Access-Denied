4e5e33445b0774c8b6396f05f7b4a0d845b78eea
VladG24 YT <vg07102004@gmail.com>
2020-01-08 21:38:03 +0300

    Create package-info.java

---
+++ src/ru/VladG24YT/ConsoleSCP/GameEngine/package-info.java
@@ -0,0 +1,23 @@
+/*static Scene[] scenesList;
+
+    static void initiateScenes() {
+        LOGGER.info("Initiating scenes...");
+        String[] scenePaths;
+        File sceneDir = new File("C:\\SCP Console\\scenes\\");
+        scenePaths = sceneDir.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".jscene");
+            }
+        });
+        File[] scenes = {};
+        for (int i = 0; i < scenePaths.length; i++) {
+            scenes[i] = new File(scenePaths[i]);
+        }
+        for (int i = 0; i < scenes.length; i++) {
+            scenesList[i].readFromFile(scenes[i]);
+        }
+    }*/
+    
+    /*GameInstance currentGI = new GameInstance();
+        initiateScenes();*/
