7b508e842dd90f34ba64c4c038b1aa9f2b2c5d8a
VladG24 YT <vg07102004@gmail.com>
2020-01-08 21:38:22 +0300

    Update ConsoleSCP.java
    Убрал поддержку движка

--- src/ru/VladG24YT/ConsoleSCP/ConsoleSCP.java
+++ src/ru/VladG24YT/ConsoleSCP/ConsoleSCP.java
@@ -25,30 +25,7 @@ public class ConsoleSCP {
 
     static Logger LOGGER;
 
-    /*static Scene[] scenesList;
-
-    static void initiateScenes() {
-        LOGGER.info("Initiating scenes...");
-        String[] scenePaths;
-        File sceneDir = new File("C:\\SCP Console\\scenes\\");
-        scenePaths = sceneDir.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".jscene");
-            }
-        });
-        File[] scenes = {};
-        for (int i = 0; i < scenePaths.length; i++) {
-            scenes[i] = new File(scenePaths[i]);
-        }
-        for (int i = 0; i < scenes.length; i++) {
-            scenesList[i].readFromFile(scenes[i]);
-        }
-    }*/
-
     public static void main(String[] args) throws IOException {
-        /*GameInstance currentGI = new GameInstance();
-        initiateScenes();*/
         new GUI(LOGGER);
     }
     
