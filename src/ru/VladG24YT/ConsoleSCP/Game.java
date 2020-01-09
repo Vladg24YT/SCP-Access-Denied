c3ae8f3acf73333090287d8cb30df08ca9eb90dc
VladG24 YT <vg07102004@gmail.com>
2020-01-09 22:39:27 +0300

    Создал Game.java
    Решил разъединить GUI и саму игру. Как только смогу - доделаю связку этих двух файлов

---
+++ src/ru/VladG24YT/ConsoleSCP/Game.java
@@ -0,0 +1,11 @@
+package ru.Vladg24YT.ConsoleSCP;
+
+public class Game implements ActionListener {
+  public Game() {
+    
+  }
+  
+  public ActionListener getActionListener() {
+    return this;
+  } 
+} 
