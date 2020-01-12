3e9327df0111054f9c1809ba5c1ca0e891adedbc
VladG24 YT <vg07102004@gmail.com>
2020-01-12 21:16:37 +0300

    Обновил на рабочую версию

--- src/ru/VladG24YT/ConsoleSCP/ConsoleSCP.java
+++ src/ru/VladG24YT/ConsoleSCP/ConsoleSCP.java
@@ -21,37 +21,14 @@ public class ConsoleSCP {
 
     static Logger LOGGER;
 
-    /*static Scene[] scenesList;
-
-    static void initiateScenes() {
-        LOGGER.info("Initiating scenes...");
-        String[] scenePaths;
-        File sceneDir = new File("C:\\SCP Console\\scenes\\");
-        scenePaths = sceneDir.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".jscene");
-            }
-        });
-        File[] scenes = {};
-        for (int i = 0; i < scenePaths.length; i++) {
-            scenes[i] = new File(scenePaths[i]);
-        }
-        for (int i = 0; i < scenes.length; i++) {
-            scenesList[i].readFromFile(scenes[i]);
-        }
-    }*/
-
     public static void main(String[] args) throws IOException {
-        /*GameInstance currentGI = new GameInstance();
-        initiateScenes();*/
-        new Game(LOGGER);
+        new GUIMaker().setVisible(true);
     }
     
     static {
         try (FileInputStream ins = new FileInputStream("C:\\SCP Console\\logs\\log.config")) {
             LogManager.getLogManager().readConfiguration(ins);
-            LOGGER = Logger.getLogger(GUI.class.getName());
+            LOGGER = Logger.getLogger(GUIMaker.class.getName());
             System.out.println("Logger initiated");
         } catch (Exception ignore) {
             ignore.printStackTrace();
